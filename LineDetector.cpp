4fbf79c970d984b8d3ab7184f0effd7f80a3c1f2
KevinHanwang <790336202@qq.com>
2021-04-24 11:04:04 +0800

    for realuse to guangyao

--- LineDetector.cpp
+++ LineDetector.cpp
@@ -1,7 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
 #include <math.h>
 #include <vector>
 #include <algorithm>
@@ -9,48 +8,59 @@
 using namespace std;
 using namespace cv;
 
-Mat srcImage, midImage, cutImage, cutImage2;
 const int height = 1084, width = 813;
-
-int g_nStructElementSize = 18; //结构元素(内核矩阵)的尺寸
-int g_nthreshold=140;
+// cutImage是梯形感兴趣区域的图片
+Mat srcImage, midImage, grayImage, cutImage, cutImage2;
+int g_nStructElementSize = 12; //形态学闭运算结构元素(内核矩阵)的尺寸
+int g_nthreshold=120;
 int g_merge = 50;
 int g_cannyLowThreshold=10;
 
+vector<Point> LineDetector(Mat src, Mat srcImage,Size srcSize);
 vector<double> line_fit(vector<Vec4i>& h);
 vector<int> kmeans(vector<int>& sample,int k);
 
-int main(int argc, char** argv)
-{
-	
-	// 载入原始图和Mat变量定义   
+int main(int argc, char** argv){
+    // 载入原始图,原始图像太大，所以对原始图像进行分辨率重设 
 	Mat src = imread(argv[1]);
     Size srcSize = Size(width, height);  //填入任意指定尺寸
     resize(src, srcImage, srcSize, 0, 0, INTER_LINEAR);
-    namedWindow("Original", 0);
-    cvResizeWindow("Original", 2000, 1200);
-    imshow("Original", src);
+    imshow("Original", srcImage);
 
+    vector<Point> lineOutput(4); 
+    lineOutput = LineDetector(src, srcImage, srcSize);
+    
+    cout << lineOutput[0].x << " " << lineOutput[1].x << " " << lineOutput[2].x << " " << lineOutput[3].x << endl; 
+    line(srcImage, Point(lineOutput[0].x,lineOutput[0].y),Point(lineOutput[1].x,lineOutput[1].y), Scalar(0,0,255), 5, CV_AA);
+    line(srcImage, Point(lineOutput[2].x,lineOutput[2].y),Point(lineOutput[3].x,lineOutput[3].y), Scalar(0,0,255), 5, CV_AA);
+    
+    imshow("result", srcImage);
+    waitKey(0);
+    return 0;
+}
+
+vector<Point> LineDetector(Mat src, Mat srcImage, Size srcSize){
+    vector<Point> twoLines(4);
     int x1l, x1r, x2l, x2r;
     int y1l, y1r, y2l, y2r;
 
-	// 进行形态学闭运算操作
-	// 定义核
-    Mat element = getStructuringElement(MORPH_RECT, Size(2*g_nStructElementSize+1,2*g_nStructElementSize+1),Point( g_nStructElementSize, g_nStructElementSize ));
-    morphologyEx(src,midImage, MORPH_CLOSE, element);
+	cvtColor(src, src, CV_BGR2GRAY);
+    GaussianBlur(src, src, Size(61, 61), 2, 2);
+    // 直方图均衡化
+    equalizeHist(src, src);
+    // 进行形态学闭运算操作
+    Mat element = getStructuringElement(MORPH_RECT, Size(2*g_nStructElementSize+1,2*g_nStructElementSize+1),
+        Point( g_nStructElementSize, g_nStructElementSize ));
+    morphologyEx(src, midImage, MORPH_CLOSE, element);
     resize(midImage, midImage, srcSize, 0, 0, INTER_LINEAR);
-
-    namedWindow("xingtaixue", 0);
-    cvResizeWindow("xingtaixue", 2000, 1200);
-    imshow("xingtaixue", midImage);
 
 	//调用HoughLinesP函数
 	vector<Vec4i> mylines;
     vector<Vec4i> mylines_left;
     vector<Vec4i> mylines_right;
 
-    cvtColor(midImage,midImage, CV_BGR2GRAY); //转化边缘检测后的图为灰度图
     GaussianBlur( midImage, midImage, Size(15, 15), 2, 2);
+    grayImage = midImage.clone();
 
 	Canny(midImage, midImage, g_cannyLowThreshold, g_cannyLowThreshold*3, 3);
 
@@ -64,26 +74,17 @@ int main(int argc, char** argv)
     fillConvexPoly(mask,PointArray,4,Scalar(255));
     bitwise_and(mask,midImage,cutImage);
 
-    namedWindow("mask", 0);
-    cvResizeWindow("mask", 2000, 1200);
-    imshow("mask", mask);
-    namedWindow("edgeDetection", 0);
-    cvResizeWindow("edgeDetection", 2000, 1200);
-    imshow("edgeDetection", cutImage);
-
 	HoughLinesP(cutImage, mylines, 1, CV_PI/180, g_nthreshold+1, 50, g_merge+1);
-    vector<int> slopeset;
+    vector<int> slopeset; //提取的直线到斜率集合
     
 	//循环遍历绘制每一条线段,斜率小于10的直接删除，并区分左右直线
 	for(auto it = mylines.begin(); it != mylines.end(); ++it){
 		Vec4i L = *it;
 		int k;
-        if(abs(L[2] - L[0]) < 1){
-            k = 150;
-        }
+        if(abs(L[2] - L[0]) < 1)
+            k = 150; //设置垂直线的斜率为150
 		else 
             k = (L[3]-L[1])/(L[2]-L[0]);
-		// putText(srcImage,to_string(k),Point(L[2], L[3]),FONT_HERSHEY_SIMPLEX,0.8,Scalar(255,23,0),2,8);
 		if(abs(k) < 10){
 			it = mylines.erase(it);
             --it;
@@ -91,7 +92,6 @@ int main(int argc, char** argv)
 		}
 		slopeset.push_back(k);
 	}
-    cout << "2" << endl;
 
     if(slopeset.size() > 1){
         vector<int> slopes = kmeans(slopeset, 2);
@@ -108,25 +108,27 @@ int main(int argc, char** argv)
                 Vec4i L = *it;
                 int k;
                 if(abs(L[2] - L[0]) < 1){
-                    k = 1000;
+                    k = 150;
                 }
                 else 
                     k = (L[3]-L[1])/(L[2]-L[0]);
-                // putText(srcImage,to_string(k),Point(L[2], L[3]),FONT_HERSHEY_SIMPLEX,0.8,Scalar(255,23,0),2,8);
-                // line( srcImage, Point(L[0], L[1]), Point(L[2], L[3]), Scalar(0,0,255), 5, CV_AA);
                 if(flag){
                     // 斜率不同符号，大斜率在左边
-                    if(k < slopeThresh)
+                    if(k < slopeThresh){
                         mylines_left.push_back(L);
-                    else
+                    }
+                    else{
                         mylines_right.push_back(L);
+                    }
                 }
                 else{
                     // 斜率同符号，大斜率在右边
-                    if(k > slopeThresh)
+                    if(k > slopeThresh){
                         mylines_left.push_back(L);
-                    else
+                    }
+                    else{
                         mylines_right.push_back(L);
+                    }
                 }
             }
 
@@ -138,75 +140,27 @@ int main(int argc, char** argv)
             if(mylines_left.size() > 0 && mylines_right.size() > 0){
                 crossPoint_y = (lineFitLeft[0]*lineFitRight[1] + lineFitLeft[1]*lineFitRight[0]) / (lineFitRight[0] - lineFitLeft[0]);
                 y1r = 0;
-                y2r = min(5*height/7, crossPoint_y);
+                y2r = min(4*height/7, crossPoint_y);
                 y1l = 0;
-                y2l = min(5*height/7, crossPoint_y);
+                y2l = min(4*height/7, crossPoint_y);
 
                 x1r = (y1r - lineFitRight[1]) / lineFitRight[0];
                 x2r = (y2r - lineFitRight[1]) / lineFitRight[0];
                 x1l = (y1l - lineFitLeft[1]) / lineFitLeft[0];
                 x2l = (y2l - lineFitLeft[1]) / lineFitLeft[0];
-
-                cout << x1r << " " << x2r << " " << x1l << " " << x2l << endl;
-                line(srcImage, Point(x1r,y1r),Point(x2r,y2r), Scalar(0,0,255), 5, CV_AA);
-                line(srcImage, Point(x1l,y1l),Point(x2l,y2l), Scalar(0,0,255), 5, CV_AA);
             }
         }
     }
+    twoLines[0].x = x1r;
+    twoLines[0].y = y1r;
+    twoLines[1].x = x2r;
+    twoLines[1].y = y2r;
+    twoLines[2].x = x1l;
+    twoLines[2].y = y1l;
+    twoLines[3].x = x2l;
+    twoLines[3].y = y2l;
 
-    imshow("line_result ", srcImage);
-
-    // 防振锤检测掩膜
-    Mat mask_damper = Mat::zeros(srcImage.size(), CV_8UC1);
-    Point PointArray2[4];
-    PointArray2[0] = Point(x1l+30, 200);
-    PointArray2[1] = Point(x1r-30, 200);
-    PointArray2[2] = Point(x2l-30 ,y2l);
-    PointArray2[3] = Point(x2r+30 ,y2r);
-    fillConvexPoly(mask_damper,PointArray2,4,Scalar(255));
-    bitwise_and(mask_damper,midImage,cutImage2);
-    imshow("mask_damper", mask_damper);
-    imshow("cutImage2", cutImage2);
-
-    vector<Vec3f> circles;
-    HoughCircles( cutImage2, circles, CV_HOUGH_GRADIENT,1.5, 150, 200, 30, 20, 50);
-    cout << circles.size() << endl;
-
-    //依次在图中绘制出圆
-    for( size_t i = 0; i < circles.size(); i++ ){
-        Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-        int radius = cvRound(circles[i][2]);
-
-        // //圆心不与输电线中心线对称的话，跳出
-        // int bias = cvRound(circles[i][0]) - mid_line;
-        // if(abs(bias) > 30) continue;
-
-        // //判断圆与直线是否太近，如果太近，那么舍弃
-        // int distance1 = pow((circles[i][0] - closedToCir[0]),2) + pow((circles[i][1] - closedToCir[1]),2);
-        // int distance2 = pow((circles[i][0] - closedToCir[2]),2) + pow((circles[i][1] - closedToCir[3]),2);
-        // int r_2 = pow(circles[i][2],2);
-        // int dis = min(distance1-r_2, distance2 -r_2);
-        // if(dis < DISTANCE) continue;
-
-        // // //通过判断圆心与最近端点的高度来筛选圆
-        // int y_bias1 = circles[i][1] - closedToCir[1];
-        // int y_bias2 = circles[i][1] - closedToCir[3];
-        // int y_bias = min(y_bias1, y_bias2);
-        // if(y_bias < circles[i][2]) continue;
-
-        //绘制圆心
-        circle( srcImage, center, 3, Scalar(0,255,0), -1, 8, 0 );
-        //绘制圆轮廓
-        circle( srcImage, center, radius, Scalar(255,0,0), 3, 8, 0 );
-        //打印圆心坐标
-        // printf("x = %d,y = %d\n",cvRound(circles[i][0]),cvRound(circles[i][1]));
-        // }
-    }
-
-    imshow("result ", srcImage);
-
-    waitKey(0);
-    return 0;
+    return twoLines;
 }
 
 //最小二乘法拟合直线
